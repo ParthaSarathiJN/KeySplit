bcce4e4cb7842f19dac4a28214d5a8d29505208d
Partha <parthasarathi.jn@gtstechlabs.com>
2024-09-21 18:20:13 +0530

    Tested plain sending of PDU Header using sockets

--- src/main/java/io/github/ParthaSarathiJN/Main.java
+++ src/main/java/io/github/ParthaSarathiJN/Main.java
@@ -1,40 +1,40 @@
-package io.github.ParthaSarathiJN;
-
-import io.github.ParthaSarathiJN.packet.GetRequest;
-import io.github.ParthaSarathiJN.packet.GetResponse;
-import io.github.ParthaSarathiJN.packet.InsertRequest;
-import io.github.ParthaSarathiJN.packet.InsertResponse;
-
-import java.nio.charset.StandardCharsets;
-import java.util.concurrent.ConcurrentHashMap;
-
-public class Main {
-
-    public static ConcurrentHashMap<byte[], byte[]> keySplit = new ConcurrentHashMap<>();
-
-    public static void main(String[] args) {
-
-        Main main = new Main();
-        main.start();
-
-    }
-
-    public void start() {
-        Process process = new Process();
-
-        String testReq = "processRequest", testResp = "processResponseggfdgfg";
-
-        InsertRequest insertRequest = new InsertRequest(testReq.getBytes(StandardCharsets.US_ASCII),
-                testResp.getBytes(StandardCharsets.US_ASCII));
-
-        InsertResponse insertResponse = (InsertResponse) process.processInsertPacket(insertRequest);
-        System.out.println("InsertResp ResponseCode:" + insertResponse.getStatus());
-
-        GetRequest getRequest = new GetRequest(testReq.getBytes(StandardCharsets.US_ASCII));
-
-        GetResponse getResponse = (GetResponse) process.processGetPacket(getRequest);
-        System.out.println("Value:" + new String(getResponse.getValueBytes(), StandardCharsets.US_ASCII) + ", ResponseCode:" + getResponse.getStatus());
-
-    }
-
-}
+//package io.github.ParthaSarathiJN;
+//
+//import io.github.ParthaSarathiJN.pdu.GetRequest;
+//import io.github.ParthaSarathiJN.pdu.GetResponse;
+//import io.github.ParthaSarathiJN.pdu.InsertRequest;
+//import io.github.ParthaSarathiJN.pdu.InsertResponse;
+//
+//import java.nio.charset.StandardCharsets;
+//import java.util.concurrent.ConcurrentHashMap;
+//
+//public class Main {
+//
+//    public static ConcurrentHashMap<byte[], byte[]> keySplit = new ConcurrentHashMap<>();
+//
+//    public static void main(String[] args) {
+//
+//        Main main = new Main();
+//        main.start();
+//
+//    }
+//
+//    public void start() {
+//        Process process = new Process();
+//
+//        String testReq = "processRequest", testResp = "processResponseggfdgfg";
+//
+//        InsertRequest insertRequest = new InsertRequest(testReq.getBytes(StandardCharsets.US_ASCII),
+//                testResp.getBytes(StandardCharsets.US_ASCII));
+//
+//        InsertResponse insertResponse = (InsertResponse) process.processInsertPacket(insertRequest);
+//        System.out.println("InsertResp ResponseCode:" + insertResponse.getStatus());
+//
+//        GetRequest getRequest = new GetRequest(testReq.getBytes(StandardCharsets.US_ASCII));
+//
+//        GetResponse getResponse = (GetResponse) process.processGetPacket(getRequest);
+//        System.out.println("Value:" + new String(getResponse.getValueBytes(), StandardCharsets.US_ASCII) + ", ResponseCode:" + getResponse.getStatus());
+//
+//    }
+//
+//}
--- src/main/java/io/github/ParthaSarathiJN/Process.java
+++ src/main/java/io/github/ParthaSarathiJN/Process.java
@@ -1,23 +1,24 @@
-package io.github.ParthaSarathiJN;
+//package io.github.ParthaSarathiJN;
+//
+//import io.github.ParthaSarathiJN.pdu.*;
+//
+//import java.nio.charset.StandardCharsets;
+//
+//import static io.github.ParthaSarathiJN.Main.keySplit;
+//import static io.github.ParthaSarathiJN.common.Constants.NO_ERROR;
+//
+//public class Process {
+//
+//    public BasePacket processGetPacket(GetRequest getRequest) {
+//        System.out.println("InsertRequest Process: Operation:" + getRequest.getOperation() + ", Key:" + new String(getRequest.getKeyBytes(), StandardCharsets.US_ASCII));
+//        return new GetResponse(keySplit.get(getRequest.getKeyBytes()), NO_ERROR);
+//    }
 
-import io.github.ParthaSarathiJN.packet.*;
-
-import java.nio.charset.StandardCharsets;
-
-import static io.github.ParthaSarathiJN.Main.keySplit;
-import static io.github.ParthaSarathiJN.common.Constants.NO_ERROR;
-
-public class Process {
-
-    public BasePacket processGetPacket(GetRequest getRequest) {
-        System.out.println("InsertRequest Process: Operation:" + getRequest.getOperation() + ", Key:" + new String(getRequest.getKeyBytes(), StandardCharsets.US_ASCII));
-        return new GetResponse(keySplit.get(getRequest.getKeyBytes()), NO_ERROR);
-    }
-
-    public BasePacket processInsertPacket(InsertRequest insertRequest) {
-        keySplit.put(insertRequest.getKeyBytes(), insertRequest.getValueBytes());
-        System.out.println(keySplit.get(insertRequest.getKeyBytes()));
-        return new InsertResponse(0);
-    }
-
-}
+//    public BasePacket processInsertPacket(InsertRequest insertRequest) {
+//        keySplit.put(insertRequest.getKeyBytes(), insertRequest.getValueBytes());
+//        System.out.println(keySplit.get(insertRequest.getKeyBytes()));
+//        return new InsertResponse(0);
+//    }
+//
+//}
+//
